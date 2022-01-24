8ed9fc9b1f91c2a09f845f84b96781da6959f1ac
Joseph Rubino <joeyrubi22@gmail.com>
2022-01-23 21:37:59 -0800

    Main fixed

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -3,13 +3,13 @@ import Main from './components/MainComponent'
 import './App.css';
 
 class App extends Component {
-  render() {
-      return (
-          <div className="App">
-              <Main />
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Main />
+            </div>
+        );
+    };
 }
 
-export  default App ;
+export default App;
