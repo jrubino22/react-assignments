e161171e48c5c6ee3ecdf158794980153a5cfc31
Joseph Rubino <joeyrubi22@gmail.com>
2022-02-08 12:40:58 -0800

    redux action

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -15,7 +15,7 @@ function RenderCampsite({ campsite }) {
         </div>
     )
 }
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -32,7 +32,7 @@ function RenderComments({ comments }) {
                         </div>
                     )
                 })}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
 
         )
@@ -55,7 +55,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         )
@@ -76,7 +80,7 @@ class CommentForm extends Component {
             author: '',
             comment: '',
             isModalOpen: false,
-              
+
         };
         this.toggleModal = this.toggleModal.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -89,8 +93,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
 
@@ -106,41 +110,41 @@ class CommentForm extends Component {
                     <ModalBody>
                         <LocalForm onSubmit={values => this.handleSubmit(values)}>
                             <div className="form-group">
-                                <Label htmlFor="rating">Rating</Label>                               
-                                    <Control.select model=".rating" id="rating" name="rating" className="form-control">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </Control.select>                                
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
                             </div>
                             <div className="form-group" >
                                 <Label htmlFor="author">Your Name</Label>
-                                <Control.text model=".author" id="author" name="author" className="form-control" 
-                                    placeholder="Your Name" 
+                                <Control.text model=".author" id="author" name="author" className="form-control"
+                                    placeholder="Your Name"
                                     validators={{
                                         minLength: minLength(2),
                                         maxLength: maxLength(15)
                                     }}
                                 />
                                 <Errors
-                                        className="text-danger"
-                                        model=".author"
-                                        show="touched"
-                                        conponent="div"
-                                        messages={{
-                                            required: 'Required ',
-                                            minLength: 'Must be at least 2 characters ',
-                                            maxLength: 'Must be 15 characters or less '
-                                        }}
-                                    />
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    conponent="div"
+                                    messages={{
+                                        required: 'Required ',
+                                        minLength: 'Must be at least 2 characters ',
+                                        maxLength: 'Must be 15 characters or less '
+                                    }}
+                                />
                             </div>
                             <div className="form-group" >
                                 <Label htmlFor="text">Comment</Label>
                                 <Control.textarea model=".text" id="text" name="text"
-                                        rows="6"
-                                        className="form-control"/>
+                                    rows="6"
+                                    className="form-control" />
                             </div>
                             <div className="form-group">
                                 <Button type="submit" color="primary">Submit</Button>
